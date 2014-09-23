5143f415842430d682d9e62551c38e2f63185c8e
Florian Klampfer <f.klampfer@gmail.com>
2014-09-23 14:51:20 +0200

    make react namespace object lowercase

--- ankor-react.js
+++ ankor-react.js
@@ -2555,9 +2555,9 @@ define('ankor/ankor-react',[
 ], function (AnkorMixin,
              initReact,
              ankor) {
-    ankor.adapters.React = {};
-    ankor.adapters.React.init = initReact;
-    ankor.adapters.React.AnkorMixin = AnkorMixin;
+    ankor.adapters.react = {};
+    ankor.adapters.react.init = initReact;
+    ankor.adapters.react.AnkorMixin = AnkorMixin;
     return ankor;
 });
 
