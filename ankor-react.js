e060cccf0d6c332a9e74cf76b91fc280dc50a561
Florian Klampfer <f.klampfer@gmail.com>
2014-09-23 15:01:23 +0200

    fix react mixin

--- ankor-react.js
+++ ankor-react.js
@@ -694,17 +694,7 @@ define('ankor/adapters/react/AnkorMixin',[
             if (!(this.props.ankorRef instanceof Ref)) {
                 throw Error("Ankor React component must have a property `ankorRef` of type `Ref`");
             }
-
-            // This is bad for performance
-            var model = this.ankorRef.getValue();
-            Object.keys(model).forEach(function (key) {
-                if (!newProps[key]) {
-                    newProps[key] = model[key];
-                }
-            });
-            
-            // new api
-            // TODO: remove old one (above) before 0.4 release
+            this.ankorRef = this.props.ankorRef;
             newProps.ankor = this.ankorRef.getValue();
         }
     }
